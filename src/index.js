61dc16c5d973123a88ca222d65f3209b7837f9b3
Daniel Urdaneta <danielurn@hotmail.com>
2021-04-14 14:38:13 -0400

    Primertest

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import morgan from 'morgan';
+//INICIALIZACIONES
+const app = express();
+
+//CONFIGURACIONES
+app.set('port', process.env.PORT || 4000);
+
+//MIDDLEWARES
+app.use(morgan('dev'));
+
+//GLOBALES
+const pnombre = 'REGISTRO DE EMPLEADOS';
+
+//RUTAS
+
+
+//ABIERTO
+
+//INICIAR SERVER
+app.listen(app.get('port', ()=>{
+    console.log('SERVIDOR INICIADO EN ', app.get('port'));
+}))
