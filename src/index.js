483960f374cf75ab14b9e98b4eb42965f3b28750
Daniel Urdaneta <danielurn@hotmail.com>
2021-04-14 16:10:29 -0400

    Servidor funcionando, aplicación preparada para mysql

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const hdbs = require('express-handlebars');
+
 //INICIALIZACIONES
 const app = express();
 
 //CONFIGURACIONES
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('.hdbs', hdbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs',
+    helpers: require('./lib/handlebars')
+
+}));
+app.set('view engine', '.hbs');
+
 
 //MIDDLEWARES
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
 //GLOBALES
+app.use((require,response,next) => {
+    
+    next();
+})
 const pnombre = 'REGISTRO DE EMPLEADOS';
 
 //RUTAS
 app.use(require('./routes'));
+app.use(require('./routes/auth'));
+app.use('/links',require('./routes/links'));
 
-//ABIERTO
+
+//PUBLICO
+app.use(express.static(path.join(__dirname, 'public')));
 
 //INICIAR SERVER
 app.listen(app.get('port'), ()=>{
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,4 @@
 const express =require('express');
 const router = express.Router();
 
-router.get('/', (require,response)=>{
-    response.send('HOLA MUNDO');
-})
-
 module.exports = router;
