0471c2b71489081dcd6b4fd0687d7c1639ca6d59
Daniel Urdaneta <danielurn@hotmail.com>
2021-04-14 23:33:49 -0400

    Bases de datos creada

---
+++ database/db.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE registro;
+USE registro;
+
+CREATE TABLE usuarios(
+    idusuario INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(40) NOT NULL,
+    usuario VARCHAR(20) NOT NULL,
+    contrasena VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE horarios(
+    idhorario INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fentrada DATE NOT NULL,
+    fsalida DATE NOT NULL,
+    horas INT(2) NOT NULL
+);
+
+CREATE TABLE empleados(
+    idempleado INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    direccion VARCHAR(120),
+    correo VARCHAR(30) NOT NULL,
+    telefono VARCHAR(15),
+    fk_idcargo INT REFERENCES cargos(idcargo)
+);
+
+CREATE TABLE departamentos(
+    iddepartamento INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    depnombre VARCHAR(20) NOT NULL,
+    nempleados INT(4) NOT NULL,
+    fk_sucursal INT REFERENCES sucursales(idsucursal)
+);
+
+CREATE TABLE cargos(
+    idcargo INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cnombre VARCHAR(20) NOT NULL,
+    nempleados INT(2) NOT NULL,
+    fk_horario INT REFERENCES horarios(idhorario),
+    fk_departamento INT REFERENCES departamentos(iddepartamento)
+);
+
+CREATE TABLE lista(
+    idmov INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    fentrada DATE NOT NULL,
+    fsalida DATE NOT NULL,
+    estado BOOLEAN NOT NULL,
+    fk_idempleado INT REFERENCES empleados(idempleado)
+);
+
+CREATE TABLE sucursales(
+    idsucursal INT(15) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    snombre VARCHAR(40) NOT NULL,
+    direccion VARCHAR(200) NOT NULL,
+    nempleados INT(5) NOT NULL
+);
